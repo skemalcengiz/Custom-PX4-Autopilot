d8582e93b2ce34969b6c0dce3572444c37bcf3b9
Said Kemal Cengiz <71980168+skemalcengiz@users.noreply.github.com>
2021-08-02 14:32:49 +0300

    Add files via upload

--- src/modules/my_module/MyModule.hpp
+++ src/modules/my_module/MyModule.hpp
@@ -1,1 +1,119 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2013-2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+#pragma once
+
+#include <float.h>
+#include <lib/mathlib/mathlib.h>
+#include <lib/matrix/matrix/math.hpp>
+
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/module_params.h>
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
+
+#include <uORB/Publication.hpp>
+#include <uORB/Subscription.hpp>
+#include <uORB/SubscriptionCallback.hpp>
+
+#include <uORB/topics/parameter_update.h>
+#include <uORB/topics/sensor_combined.h>
+#include <uORB/topics/my_topic.h>
+
+using matrix::Vector3f;
+using namespace time_literals;
+
+class MyModule : public ModuleBase<MyModule>, public ModuleParams, public px4::WorkItem
+{
+private:
+    void Run() override;
+    void update_parameters(bool force = false);
+    bool upload();
+
+    DEFINE_PARAMETERS(
+        (ParamFloat<px4::params::MY_ACC_X>) _param_my_acc_x,
+        (ParamFloat<px4::params::MY_ACC_Y>) _param_my_acc_y,
+        (ParamFloat<px4::params::MY_ACC_Z>) _param_my_acc_z,
+        (ParamInt<px4::params::MY_COUNTER>) _param_my_counter,
+        (ParamBool<px4::params::MY_BOOLEAN>) _param_my_boolean
+    );
+
+    uORB::SubscriptionInterval _parameter_update_sub{ORB_ID(parameter_update), 1_s};
+    uORB::SubscriptionCallbackWorkItem _sensor_combined_sub{this, ORB_ID(sensor_combined)};
+
+    uORB::Publication<my_topic_s> _mt_pub{ORB_ID(my_topic)};
+
+    hrt_abstime	_last_time{0}; 
+    Vector3f _accel;
+    const float _dt_min = 0.00001f;
+	const float _dt_max = 0.02f;
+    int cnt = 0;
+
+public:
+    MyModule();
+    ~MyModule() override = default;
+
+	static int task_spawn(int argc, char *argv[]);
+	static int custom_command(int argc, char *argv[]);
+	static int print_usage(const char *reason = nullptr);
+	bool init();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
